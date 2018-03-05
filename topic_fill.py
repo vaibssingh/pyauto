873b769c7c4a00a13557c800e824bc6e2f3e44d6
vaibssingh <singhvaibhav592@gmail.com>
2018-03-05 15:46:16 +0530

    fix

--- topic_fill.py
+++ topic_fill.py
@@ -6,23 +6,25 @@ subtopic = (1120, 532)
 saveButton = (1220, 295)
 print("Entering info...")
 time.sleep(5)
+
+pyautogui.click(nameField)
+print('click!')
 try:
-    while i < 4:
-        pyautogui.click(nameField)
-        print('click!')
+    while i < 18:
         # gh = random.choice(('u'))
         # pyautogui.press([gh])
-        pyautogui.press('f')
+        pyautogui.press('u')
         pyautogui.press('tab', presses= 2)
         print('tabbed')
-        j = 0
-        sub = ["Classification of hormones", "Mechanism of hormone action", "Structure and functions of pituitary gland", "Thyroid gland", "Parathyroid gland", "Adrenal gland", "Pancreas", "Pineal gland", "Thymus and their disorders"]
-        for topic in sub:    
-            pyautogui.typewrite(sub[j])
-            pyautogui.press('tab')
-            j += 1 
         time.sleep(1)
-        pyautogui.scroll(-1)
+        # j = 0
+        # sub = ["Classification of hormones", "Mechanism of hormone action", "Structure and functions of pituitary gland", "Thyroid gland", "Parathyroid gland", "Adrenal gland", "Pancreas", "Pineal gland", "Thymus and their disorders"]
+        # for topic in sub:    
+        #     pyautogui.typewrite(sub[j])
+        #     pyautogui.press('tab')
+        #     j += 1 
+        # time.sleep(1)
+        # pyautogui.scroll(-1)
         # time.sleep(1)
         i += 1
 except KeyboardInterrupt:
